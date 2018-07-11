2d992e4105b0a42a4ca648fcfde924a2250ce62b
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2018-07-11 16:20:54 +0300

    loading errors

--- FirestoreHelper/Helper.swift
+++ FirestoreHelper/Helper.swift
@@ -12,25 +12,26 @@ extension Firestore {
 
 	// MARK: - Paginated Lists
 
-	public static func getList<T:Codable>(from query:Query,
-										  cursor:DocumentSnapshot?,
-										  limit:UInt,
-										  setup:QuerySetupBlock? = nil,
-										  _ completion: @escaping ([T], DocumentSnapshot?) -> Void) {
+	public static func getList
+		<T:Codable>(from query:Query,
+					cursor:DocumentSnapshot?,
+					limit:UInt,
+					setup:QuerySetupBlock? = nil,
+					_ completion: @escaping ([T], DocumentSnapshot?, Error?) -> Void) {
 		var query = query.limit(to: Int(limit))
 		if let setup = setup { query = setup(query) }
 		if let cursor = cursor { query = query.start(afterDocument: cursor) }
-		query.getDocuments { snapshot, _ in
-			completion(snapshot?.getArray(of: T.self) ?? [], snapshot?.documents.last)
+		query.getDocuments { snapshot, error in
+			completion(snapshot?.getArray(of: T.self) ?? [], snapshot?.documents.last, error)
 		}
 	}
 
 	// MARK: - Lists
 
 	public static func getList<T:Codable>(from query:Query,
-										  _ completion: @escaping ([T]) -> Void) {
-		query.getDocuments { snapshot, _ in
-			completion(snapshot?.getArray(of: T.self) ?? [])
+										  _ completion: @escaping ([T], Error?) -> Void) {
+		query.getDocuments { snapshot, error in
+			completion(snapshot?.getArray(of: T.self) ?? [], error)
 		}
 	}
 
@@ -44,16 +45,16 @@ extension Firestore {
 	}
 
 	public static func get<T:Codable>(from document: DocumentReference,
-									  _ completion: @escaping (T?) -> Void) {
-		document.getDocument { snapshot, _ in
-			completion(snapshot?.getValue(T.self))
+									  _ completion: @escaping (T?, Error?) -> Void) {
+		document.getDocument { snapshot, error in
+			completion(snapshot?.getValue(T.self), error)
 		}
 	}
 
 	public static func get<T:Codable>(from collection:CollectionReference,
 									  with id:String,
-									  _ completion: @escaping (T?) -> Void) {
-		guard id.count > 0 else { return completion(nil) } // protection from crash
+									  _ completion: @escaping (T?, Error?) -> Void) {
+		guard id.count > 0 else { return completion(nil, nil) } // protection from crash
 		get(from: collection.document(id), completion)
 	}
 }
